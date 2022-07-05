925c2c1dcb94834fd140375a4e76798842244fe6
Yash Oza <yashmichael@gmail.com>
2022-07-05 09:34:01 -0400

    Initial commit

--- dist/App.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface GameProps {
-    xFieldsCount?: number;
-    yFieldsCount?: number;
-    bombsCount?: number;
-}
-export declare const Minesweeper: React.FC<GameProps>;
-export {};
